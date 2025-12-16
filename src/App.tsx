1e7465dadec3a724ccc57ff7650b05cba679e74e
devrivzy <rizvyhq1@gmail.com>
2025-12-17 02:49:39 +0600

    chore: Update build process with new components and imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import About from "./pages/About/About";
 import Home from "./pages/Home/Home";
+import { AppSidebar } from "./components/app-sidebar";
 
 export function App() {
   return (
     <Routes>
-      <Route index path="/" element={<Home />} />
+      <Route index element={<Home />} />
+      <Route  path="/chat" element={<AppSidebar />} />
       <Route path="about" element={<About />} />
         
     </Routes>
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
+import { SidebarProvider } from "./components/ui/sidebar.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
+  <SidebarProvider>
+    <div className="max-w-7xl mx-auto">
+
     <App />
+    </div>
+  </SidebarProvider>
   </BrowserRouter>
 );
