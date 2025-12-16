32fc654623978c8cf5000b214785567c30c685b2
devrivzy <rizvyhq1@gmail.com>
2025-12-17 02:12:45 +0600

    feat: Add React Router for client-side routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import { ComponentExample } from "@/components/component-example";
+import { Route, Routes } from "react-router";
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
 
 export function App() {
-return <ComponentExample />;
+  return (
+    <Routes>
+      <Route index path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Routes>
+  );
 }
 
 export default App;
